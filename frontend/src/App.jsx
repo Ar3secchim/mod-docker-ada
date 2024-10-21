489699475c24d36aba2fc3ff7a7f6dde43f709e7
Renara Secchim <renarasecchim@gmail.com>
2024-10-21 10:17:59 -0300

    Add frontend files and configurations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1>Paginas de eventos</h1>
+      </header>
+
+      <section>
+        <Card />
+      </section>
+    </>
+  );
+}
+
+export default App;
